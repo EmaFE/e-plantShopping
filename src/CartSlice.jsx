3172f5a67dd768f537de29d197915593af35384b
EmaFE <floreaemaelena@gmail.com>
2025-08-02 14:46:10 +0100

    Update CartSlice.jsx
    defined reducer functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem){
+        existingItem.quantity++;
+      } else {
+        state.items.push({name, image, cost})
+      }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+    removeItem: (state, action) => {
+      state.items.filter(item => item.name !== action.payload.name)
+    },
+
+    updateQuantity: (state, action) => {
+      const {name, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem){
+        existingItem.quantity++;
+      }
     },
   },
 });
